e38c721359e39740e820f7d17a34a34197253dee
Rita <ritanikky205@gmail.com>
2025-11-11 15:57:20 +0100

    MY classwork

---
+++ README.md
@@ -0,0 +1,1 @@
+This is class work and assignments
---
+++ index.py
@@ -0,0 +1,147 @@
+name_class ="Hello Rita"
+
+print(name_class)
+
+name= "Rita"
+age=23
+
+print(f"my name is {name}\nI am {age} years old")
+
+name= ["promise", "Rita", "Success", "Innocent"]
+a, b, c, d = name
+
+print(a)
+print (b)
+print (c)
+print(d)
+
+#month= ["jan", "Feb", "March"]
+#x, y, z =month
+
+#print(x)
+#print(y)
+#print(z)
+
+print("my name is Rita\nI am 23 years old\nI am a web developer")
+
+Name_Class_Group= "Rita"
+print(f"my name is {Name_Class_Group}\nI am a web developer")
+
+x= "Rita "
+y= "is a good girl "
+z= "always "
+
+print(x+y+z)
+
+x = "Rita"
+
+def myfunc():
+    print("my name is " + x)
+
+myfunc()
+
+def myfunc():
+    global baller
+    baller = "fantastic"
+
+myfunc()
+
+print("python is " + baller)
+
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+a = """I am a fan of football\nI love Chelsea football club\nmy blood bleeds blue."""
+print(a)
+a='''I am e tech enthuasist\nA web developer\nA coder.'''
+print(a)
+
+a = "Hello World"
+print(a[8])
+
+for x in "apple":
+    print(x)
+
+for y in "melon":
+    print(y)
+
+   # a = "Hello World"
+   # print(len(a))
+
+a = ['mango', 'apple', 'cashew']
+print(len(a))
+
+
+txt = "The best things in life are free!"
+print("bee" in txt)
+
+b = "Hello, World"
+print(b[3:5])
+
+#slicing from the start
+
+name = "Hello, World"
+print(name[:5])
+
+name = "Rita, World"
+print(name[5:])
+print(name.upper())
+print(name.lower())
+print(name.strip())
+
+print(name.replace("R", "Z"))
+
+print(name.split(","))
+
+#concatenation string
+
+nan = 'Come '
+vin = 'here '
+can = 'now'
+dao = nan + vin + can
+
+print(dao)
+nan = 'Come'
+vin = 'here'
+can = 'now'
+dao = nan + " "+ vin +" "+ can
+
+print(dao)
+
+price = 30
+print(f"I need {price:.3f} money")
+
+#format spring outside print function
+
+txt = f"I need {20} houses"
+print(txt)
+
+txt = f"I need {20 * 3} houses"
+print(txt)
+
+test = "We are so-called \"Vikings\" from the North."
+print(test)
+
+
+Rita = "I have taken my lunch"
+x = Rita.capitalize()
+print (x)
+
+x = Rita.casefold()
+print (x)
+
+x = Rita.center(5)
+print (x)
+
+x = Rita.count("h")
+print (x)
+
+x = Rita.encode()
+print (x)
+
+x = Rita.endswith("h")
+print (x)
+
+x = Rita.expandtabs("5")
+print (x)
+    
